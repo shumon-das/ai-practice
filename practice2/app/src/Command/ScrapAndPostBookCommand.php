04a890c1da6c4675621f0580abb7c2cc8ffe92cb
shumonsb <shumonsb@gmail.com>
2025-10-30 17:26:45 +0600

    scrapping links is completed

--- practice2/app/src/Command/ScrapAndPostBookCommand.php
+++ practice2/app/src/Command/ScrapAndPostBookCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -16,23 +17,34 @@ use Symfony\Component\DomCrawler\Crawler;
 )]
 class ScrapAndPostBookCommand extends Command
 {
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        parent::__construct();
+        $this->params = $params;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $directory = $this->getParameter('kernel.project_dir') . '/books';
+        $directory = $this->params->get('kernel.project_dir') . '/books';
+        // $directory = $this->params('kernel.project_dir') . '/books';
         if (!is_dir($directory)) {
             $io->error('Books directory not found');
         }
+        dump($directory);
 
         // Get all .txt files
         $files = glob($directory . '/*.txt');
 
         if (!$files) {
-            return new JsonResponse(['message' => 'No .txt files found in books directory']);
+            $io->warning(['message' => 'No .txt files found in books directory']);
         }
 
         $url = 'https://bidyakolpo.in/jashim-uddin-bangla-golpo-bengali-story/page/2/';
 
+        return Command::SUCCESS;
     }
 }
--- practice2/app/src/Command/ScrapLinksCommand.php
+++ practice2/app/src/Command/ScrapLinksCommand.php
@@ -4,7 +4,9 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -25,16 +27,37 @@ class ScrapLinksCommand extends Command
         $this->params = $params;
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('filename', InputArgument::OPTIONAL, 'Filename')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            // ->addArgument('url', InputArgument::OPTIONAL, 'Url')
+            // ->addOption('option2', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $url = 'https://bidyakolpo.in/premendra-mitra-bangla-golpo-bengali-story/';
+        $filename = $input->getArgument('filename');
+        if (!$filename) {
+            $io->error('Filename is Required');
+            return Command::INVALID;
+        }
+
+        // $url = $input->getArgument('url');
+        // if (!$url) {
+        //     $io->error('Url is Required');
+        //     return Command::INVALID;
+        // }
+        $url = 'https://bidyakolpo.in/sharadindu-bandyopadhyay-bangla-golpo-bengali-story/page/';
 
         $linksDir = $this->params->get('kernel.project_dir') . '/links';
         if (!is_dir($linksDir)) {
             mkdir($linksDir, 0777, true);
         }
-        $filename = $linksDir .'/premendra_mitra.txt';
+        $filename = $linksDir . '/' . $filename . '.txt';
 
         try {
             $client = HttpClient::create([
@@ -47,17 +70,26 @@ class ScrapLinksCommand extends Command
                 ]
             ]);
 
-            $response = $client->request('GET', $url);
-            $html = $response->getContent();
+            $allLinks = [];
+            for ($i=1; $i < 100; $i++) {
+                $pageUrl = $url . $i;
 
-            $crawler = new Crawler($html);
+                $response = $client->request('GET', $pageUrl);
+                if (404 === $response->getStatusCode() || 200 !== $response->getStatusCode()) {
+                    $io->success("book page end at $i");
+                    break;
+                }
 
-            // ✅ Get all links
-            $links = $crawler->filter('.entry-title a')->each(fn($node) => $node->attr('href'));
-            $links = array_filter($links); // remove null
-            file_put_contents($filename, implode("\n", $links));
+                $html = $response->getContent();
+                $crawler = new Crawler($html);
+                $links = $crawler->filter('.entry-title a')->each(fn($node) => $node->attr('href'));
+                $links = array_filter($links); // remove null
+                $allLinks = array_merge($allLinks, $links);
+            }
 
-            $io->success('links scraped');
+            file_put_contents($filename, implode("\n", $allLinks));
+
+            $io->success('All links scraped');
 
         } catch (\Exception $e) {
             $io->error('Error: ' . $e->getMessage());
